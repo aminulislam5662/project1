d90007bdfc57c4c3789e22df4573ca7910a46770
aminulislam5662 <aminulislam5662@gmail.com>
2022-06-22 14:22:54 +0600

    test

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='pythonProjects',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='Walton',
+    author_email='',
+    description=''
+)
